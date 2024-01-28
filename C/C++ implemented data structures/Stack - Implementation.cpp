8bdec96b76f24068e04b8624198ceed2da9d4e8b
Kevin-Bonet <kevinjbonet@gmail.com>
2024-01-28 14:13:36 -0400

    stack implementation

---
+++ C/C++ implemented data structures/Stack - Implementation.cpp
@@ -0,0 +1,38 @@
+//Stack - Array based implementation
+#include<stdio.h>
+#define MAX_SIZE 101
+int A[MAX_SIZE];
+int top = -1;
+void Push(int x){
+    if(top == MAX_SIZE-1) {
+        printf("Error: Stack overflow\n");
+        return;
+    }
+    top++;
+    A[top] = x;
+    // or use A[++top] = x;
+}
+void Pop(){
+     if(top == -1) {
+        printf("Error: No element to pop\n");
+        return;
+    }
+    top--;
+}
+int Top() {
+    return A[top];
+}
+void Print() {
+    int i;
+    printf("Stack: ");
+    for(i=0;i<=top;i++)
+        printf("%d ",A[i]);
+    printf("\n");
+}
+int main() {
+    Push(2);Print();
+    Push(5);Print();
+    Push(10);Print();
+    Pop();Print();
+    Push(12);Print();
+}
