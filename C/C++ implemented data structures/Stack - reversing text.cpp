be38e04d6270ada697368baaac33655bffbdca7f
Kevin-Bonet <kevinjbonet@gmail.com>
2024-01-28 17:36:53 -0400

    stack reversing text

---
+++ C/C++ implemented data structures/Stack - reversing text.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stack> //stack from standard template library (STL)
+#include<stdio.h>
+#include <string.h>
+using namespace std;
+
+
+
+void Reverse(char *C, int n) {
+    stack<char> S;
+    //loop for push
+    for(int i=0; i<n; i++) {
+        S.push(C[i]);
+    }
+    //loop for pop
+    for(int i=0; i<n; i++) {
+        C[i] = S.top();
+        S.pop();
+    }
+}
+
+int main() {
+    char C[51];
+    printf("Enter a string: ");
+    fgets(C, sizeof(C), stdin);
+    Reverse(C,strlen(C));
+    printf("Output = %s",C);
+}
